e71013720e15620d74ef4726e1682732efbe55a8
Vinicius Silva <vinicius768@msn.com>
2017-04-02 15:30:37 -0300

    Initial files and structure commit

---
+++ src/main/java/br/com/viniciusls/HttpWorkshopApplication.java
@@ -0,0 +1,12 @@
+package br.com.viniciusls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpWorkshopApplication.class, args);
+	}
+}
